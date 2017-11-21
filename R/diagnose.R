c58b3f2b1a8689d3d67a436e83320072cf3e6bbd
Robert Schnitman <robertschnitman@gmail.com>
2017-11-21 04:12:54 -0600

    diagnose.R - new inputs for glm objects

--- R/diagnose.R
+++ R/diagnose.R
@@ -1,6 +1,8 @@
 #' Graphically diagnose model residuals.
 #'
 #' @param model An lm or glm object.
+#' @param fit_type String. Default is "response". Type of fitted values to use based on options in predict().
+#' @param residual_type String. Default is "response". Type of residuals values to use based on options in resid().
 #' @return 2x2 charts similar to plot(model.lm): 2 scatter plots and 2 histograms of residuals and "residuals margin,"
 #' which is the residuals as a percentage of the actual dependent variable values.
 #' @examples
@@ -27,28 +29,52 @@
 
 ##### === BEGIN === #####
 
-diagnose <- function(model) {
+diagnose <- function(model, fit_type = 'response', residual_type = 'response') {
   ### Set up fitted values, residuals, and 2x2 frame ###
-  fit <- predict(model)
-  res <- resid(model)
-  act <- model.frame(model)[, 1]  # Actual values from the depdendent variable.
-  pct <- (res/act)*100            # "Margin": residuals as a % of actual values.
+  fit <- predict(model, type = fit_type)
+  res <- resid(model, type = residual_type)
+  act <- model.frame(model)[, 1]           # Actual values from the depdendent variable.
+  pct <- (res/act)*100                     # "Margin": residuals as a % of actual values.
 
+  fitr       <- cbind(fit, res, act, pct)
+  fitr.noinf <- subset(fitr, !is.infinite(pct))
+  diff       <- NROW(fitr) - NROW(fitr.noinf)
+
+  warn1      <- 'row was deleted due to infinite values.'
+  warn2      <- ' rows were deleted due to infinite values.'
+  if (diff == 1) {
+    warning(diff, warn1, sep = ' ')
+  } else {
+    warning(diff, warn2, sep = ' ')
+  }
+
+  ### Graph is modified based on fit_type and residual_type specifications. ###
+  family <- model$family[1]               # To determine whether graph shows fitted values OR predicted probabilities.
+  pp   <- 'Predicted Probabilities'
+  fv   <- 'Fitted Values'
+  vspp <- 'vs. Predicted Probabilities'
+  vsfv <- 'vs. Fitted Values'
+
+  ### Set up 2x2 frame ###
   par(mfrow = c(2,2))
 
   ### Figure 1 - Residuals vs. Fitted ###
-  scatter.smooth(x    = fit,
-                 y    = res,
-                 xlab = 'Fitted Values',
+  scatter.smooth(x    = fitr.noinf[ , 'fit'],
+                 y    = fitr.noinf[ , 'res'],
+                 xlab = ifelse(fit_type == 'response' & family == 'binomial', pp, fv),
                  ylab = 'Residuals',
-                 main = 'Residuals vs. Fitted Values')
+                 main = ifelse(fit_type == 'response' & family == 'binomial',
+                               paste('Residuals', vspp, sep = ' '),
+                               paste('Residuals', vsfv, sep = ' ')))
 
   ### Figure 2 - Residuals, % vs. Fitted ###
-  scatter.smooth(x    = fit,
-                 y    = pct,
-                 xlab = 'Fitted Values',
+  scatter.smooth(x    = fitr.noinf[ , 'fit'],
+                 y    = fitr.noinf[ , 'pct'],
+                 xlab = ifelse(fit_type == 'response' & family == 'binomial', pp, fv),
                  ylab = 'Residuals Margin (%)',
-                 main = 'Residuals Margin (%) vs. Fitted Values')
+                 main = ifelse(fit_type == 'response' & family == 'binomial',
+                               paste('Residuals Margin (%)', vspp, sep = ' '),
+                               paste('Residuals Margin (%)', vsfv, sep = ' ')))
 
   ### Figure 3 - Distribution of Residuals ###
   hist(x = res, xlab = 'Residuals',            main = 'Distribution of Residuals')
