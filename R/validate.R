b2da3f0cd615e91a320cf4bc8d7e1ef39b23e5e2
Robert Schnitman <robertschnitman@gmail.com>
2017-12-02 04:29:15 -0600

    validate.R - roxygen UTF-8 issue

--- R/validate.R
+++ R/validate.R
@@ -2,7 +2,7 @@
 #'
 #' @param model An lm or glm object.
 #' @return Vector (column). Includes F-statistic, R-squared, RMSE, and others.
-#' @details The broom library's glance() had a vague label for the F statistic (simply "statistic") and lacked the pseudo R-squared, which is commonly based on McFadden's version (i.e. 1 - (residual deviance ÷ null deviance)).
+#' @details The broom library's glance() had a vague label for the F statistic (simply "statistic") and lacked the pseudo R-squared, which is commonly based on McFadden's version (i.e. 1 - (residual deviance / null deviance)).
 #' While the same function is friendly for data frames, it's wide form is cumbersome for quickly ascertaining model validity. Thus, validate() produces similar output as a column vector. Those who wish to have the values in broom's format can always transpose the vector.
 #' @examples
 #' model.lm <- lm(data = mtcars, formula = mpg ~ wt + gear)
