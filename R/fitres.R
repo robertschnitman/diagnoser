f0b3818e036db8ce9ef1820d8029a301c51982c0
Robert Schnitman <robertschnitman@gmail.com>
2017-12-12 21:41:14 -0600

    fitres.R - roxygen, residual_type
    1. Specify condition for processing NLS objects.
    2.  Allow flexibility of residual type.

--- R/fitres.R
+++ R/fitres.R
@@ -2,14 +2,24 @@
 #'
 #' @param model An lm, glm, or nls(model = TRUE) object.
 #' @param data A data frame. If not specified, then the original data is used (i.e. model.frame(model)).
-#' @param type String. Prediction type depends on whether the object is lm ('response', 'terms') or glm ('link', 'response', 'terms'). (See ?predict.lm and ?predict.glm for details).
+#' @param fit_type String. Prediction type. See ?predict for details.
+#' @param residual_type String. See ?resid for details.
 #' @return A matrix.
 #' @details fitres() creates a matrix of the fitted values, residuals, and residuals as a proportion (percent) of the actual dependent variable values based on an OLS model or GLM.
 #' @examples
+#' # OLS case
 #' model.lm <- lm(data = mtcars, formula = mpg ~ wt + gear)
 #' fitres(model.lm, type = 'response')
+#'
+#' # NLS case
+#' require(graphics)
+#' DNase1    <- subset(DNase, Run == 1)
+#' fm1DNase1 <- nls(density ~ SSlogis(log(conc), Asym, xmid, scal), DNase1)
+#' fitres(fm1DNase1)
+#'
 #' @section Warning:
 #' NLS objects will only work if "model = TRUE" is specified in the original NLS function.
+#'
 #' @seealso \url{https://github.com/robertschnitman/diagnoser}
 
 #####################################################################################
@@ -29,7 +39,7 @@
 ###  Schnitman, Robert (2017). fitres.r. https://github.com/robertschnitman/diagnoser
 #####################################################################################
 
-fitres <- function(model, type = 'response') {
+fitres <- function(model, fit_type = 'response', residual_type = 'response') {
 
   ### Type-checking ###
   lgm_condition <- class(model) == 'lm' | class(model)[1] == 'glm'
@@ -38,7 +48,7 @@ fitres <- function(model, type = 'response') {
   stopifnot(lgm_condition | nls_condition)
 
   ### Set up fitres matrix ###
-  fit             <- predict(model, type = type)
+  fit             <- predict(model, type = fit_type)
   actual          <- if (lgm_condition) {
     model.frame(model)[1]
 
@@ -48,10 +58,10 @@ fitres <- function(model, type = 'response') {
   }
 
   residual        <- if (lgm_condition) {
-    resid(model)
+    resid(model, type = residual_type)
 
   } else if (nls_condition) {
-    r                <- resid(model)
+    r                <- resid(model, residual_type)
     attr(r, 'label') <- NULL
     r
 
